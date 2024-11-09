a0e150b2e1d17243c57c99da06270274a270f2ac
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2024-11-08 20:31:03 -0500

    this version doesn't really work. I'm only able to copy the 4 first elements, but the rest of the elements is garbage

--- chisel/NeuralNetwork.scala
+++ chisel/NeuralNetwork.scala
@@ -9,43 +9,46 @@ import _root_.circt.stage.ChiselStage
 
 class NeuralNetwork extends Module {
   // AXI-Stream Connection
-  val sAxis = Wire(new AxiStreamSlaveIf(8))
-  IO(new AxiStreamExternalIf(8)).suggestName("s_axis").connect(sAxis)
+  val sAxis = Wire(new AxiStreamSlaveIf(16))
+  IO(new AxiStreamExternalIf(16)).suggestName("s_axis").connect(sAxis)
 
-  val mAxis = Wire(new AxiStreamMasterIf(8))
-  IO(Flipped(new AxiStreamExternalIf(8))).suggestName("m_axis").connect(mAxis)
-  
-  val input_data = VecInit(1.U, 2.U, 4.U, 8.U, 12.U, 15.U, 10.U, 22.U) // works with an
+  val mAxis = Wire(new AxiStreamMasterIf(16))
+  IO(Flipped(new AxiStreamExternalIf(16))).suggestName("m_axis").connect(mAxis)
+
   val sending = RegInit(false.B)
-  val receiveCounter = RegInit(0.U(3.W))
-  val sendCounter = RegInit(0.U(3.W))
-  val sum = RegInit(0.U(9.W))
+  val sendCounter = RegInit(0.U(9.W))
+  val receiveCounter = RegInit(0.U(9.W))
+  val input_data = RegInit(VecInit(Seq.fill(401)(0.S(16.W))))
 
-  sAxis.tready := RegInit(true.B)
+  sAxis.tready := RegInit(true.B) // ready to receive some data
+
   mAxis.data.tvalid := RegInit(false.B)
   mAxis.data.tlast := RegInit(false.B)
-  mAxis.data.tdata := RegInit(0.U)
-  mAxis.data.tkeep := "b1".U
+  mAxis.data.tdata := RegInit(0.S)
+  mAxis.data.tkeep := RegInit("b1".U)
 
-  when(sAxis.data.tvalid) {
+  when(sAxis.data.tvalid) { // the data is valid
+    input_data(receiveCounter) := sAxis.data.tdata
     receiveCounter := receiveCounter + 1.U
-    when(sAxis.data.tlast) {
+    when(sAxis.data.tlast) { // the last data is here
       sending := true.B
-      sAxis.tready := false.B
+      sAxis.tready := false.B // no longer ready to receive some data
     }
   }
-  when(sending && mAxis.tready) {
-    mAxis.data.tvalid := true.B
-    mAxis.data.tlast := false.B
-    mAxis.data.tdata := input_data(sendCounter)
-    sendCounter := sendCounter + 1.U
-    
+  when(sending && mAxis.tready) { // when the receiver is ready
+    mAxis.data.tvalid := true.B // it's true
+    mAxis.data.tlast := false.B // not the last
+    mAxis.data.tdata := input_data(sendCounter) //
+  
     when(sendCounter === receiveCounter - 1.U) {
       mAxis.data.tlast := true.B
       mAxis.data.tvalid := false.B
       sending := false.B
       receiveCounter := 0.U
       sendCounter := 0.U
+      sAxis.tready := true.B
+    }.otherwise {
+      sendCounter := sendCounter + 1.U
     }
   }
 }
